2922f4f90fd75ac718e13de6f76a4d136e86efd6
Bhavya Reddy <bhavyaaleti05@gmail.com>
2025-05-07 15:56:42 +0530

    Update signup API URLs to Render deployment

--- signup.js
+++ signup.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         try {
             console.log('Sending signup request...');
-            const response = await fetch('http://localhost:5001/signup', {
+            const response = await fetch('https://nutritech-auth.onrender.com/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
